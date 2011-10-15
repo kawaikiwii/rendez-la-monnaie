683d52517720fa71a0a35313d788148cdb72a883
Stephane PAJOT <kawaikiwii@gmail.com>
2011-10-15 20:59:12 +0200

    Correction template showsuccess goodDeal

--- apps/frontend/modules/goodDeal/templates/showSuccess.php
+++ apps/frontend/modules/goodDeal/templates/showSuccess.php
@@ -15,15 +15,15 @@
       <td><?php //echo $rlm_good_deal->getType() ?></td>
     </tr>-->
     <tr>
-      <th>Content:</th>
+      <th>Description:</th>
       <td><?php echo $gd->getContent() ?></td>
     </tr>
     <tr>
-      <th>Created at:</th>
+      <th>Créé le:</th>
       <td><?php echo $gd->getCreatedAt() ?></td>
     </tr>
     <tr>
-      <th>Updated at:</th>
+      <th>Mis à jour le:</th>
       <td><?php echo $gd->getUpdatedAt() ?></td>
     </tr>
   </tbody>
@@ -31,7 +31,7 @@
 
 <hr />
 
-<a href="<?php echo url_for('goodDeal/edit?id='.$gd->getId()) ?>">Edit</a>
+<a href="<?php echo url_for('goodDeal/edit?id='.$gd->getId()) ?>">Editer</a>
 &nbsp;
-<a href="<?php echo url_for('goodDeal/index') ?>">List</a>
+<a href="<?php echo url_for('goodDeal/index') ?>">Retour à la recherche</a>
 <?php endforeach;?>
